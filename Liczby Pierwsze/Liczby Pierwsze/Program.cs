a676941858dd83c8e044910d0fea8117f6e6b8c0
Ozyrys24 <48652183+Ozyrys24@users.noreply.github.com>
2019-10-15 08:51:42 +0200

    Add "Liczby pierwsze"
    Add spoj project "Liczby Pierwsze"

---
+++ Liczby Pierwsze/Liczby Pierwsze/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Liczby_Pierwsze
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dataSum;
+            dataSum = int.Parse(Console.ReadLine());
+
+            for (int j = 0; j < dataSum; j++)
+            {
+                int divisor = 2;
+                int number;
+                number = int.Parse(Console.ReadLine());
+
+                if (number == 1)
+                {
+                    Console.WriteLine("NIE");
+                    continue;
+                }
+
+                while (true)
+                {
+                    if (number % divisor == 0 && number != divisor)
+                    {
+                        Console.WriteLine("NIE");
+                        break;
+                    }
+
+                    if (divisor >= number)
+                    {
+                        Console.WriteLine("TAK");
+                        break;
+                    }
+
+                    divisor++;
+                }
+            }
+        }
+    }
+}
